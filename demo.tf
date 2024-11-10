8bd4603900d50346a2ef4b72647c10a31f6064d0
thanhgit <thanh29695@gmail.com>
2024-11-11 00:15:04 +0700

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -4,6 +4,6 @@ provider "null" {
 resource "null_resource" "example" {
   # This resource does nothing
   provisioner "local-exec" {
-    command = "echo 'Hello, World!'"
+    command = "echo 'Hello, C++ World!'"
   }
 }
