d0e99f9525742d8fbaea46abdf7a76ea87ccd809
thanhgit <thanh29695@gmail.com>
2024-11-11 14:54:00 +0700

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -15,3 +15,10 @@ resource "null_resource" "example1" {
   }
 }
 
+resource "null_resource" "example2" {
+  # This resource does nothing
+  provisioner "local-exec" {
+    command = "echo 'Hello, java World!'"
+  }
+}
+
