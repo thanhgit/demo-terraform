82a8f0c537e3e3c35b2b7a17f65e26f800e1f0aa
thanhgit <thanh29695@gmail.com>
2024-11-23 15:30:40 +0700

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -14,11 +14,3 @@ resource "null_resource" "example1" {
     command = "echo 'Hello, C# World!'"
   }
 }
-
-resource "null_resource" "example2" {
-  # This resource does nothing
-  provisioner "local-exec" {
-    command = "echo 'Hello, java World!'"
-  }
-}
-
